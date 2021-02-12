7e44b222bc77219dc1673c7d5fa76986267f2482
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-02-12 07:03:22 +0600

    Arraning Coins

---
+++ 441_Arranging_Coins.java
@@ -0,0 +1,11 @@
+    public int arrangeCoins(int n) {
+        int i = 1;
+        int res = 0;
+        while(n >= i){
+            n -= i;
+            res++;
+            i++;
+        }
+        return res;
+    }
+}
