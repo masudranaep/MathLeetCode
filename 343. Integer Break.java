9a78b5837ffd722ad0e6bc1adcba3de66a0c8cf7
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-06-28 08:21:28 +0600

    Dynamic

---
+++ 343. Integer Break.java
@@ -0,0 +1,10 @@
+    public int integerBreak(int n) {
+        int[] dp = new int[n+1]; dp[2] = 1;
+        for(int i = 3; i <= n; ++i){
+            for(int j = 1; j <= i /2; ++j){
+                dp[i] = Math.max(dp[i], j * Math.max(i - j, dp[i -j]));
+            }
+        }
+        return dp[n];
+    }
+}
